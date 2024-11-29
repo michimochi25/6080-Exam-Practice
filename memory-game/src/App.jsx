073bb1ca1be8efd9238592158d86f2c8cb1d4e57
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2024-11-29 13:34:11 +1100

    setup basic layout for memory game

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import p1 from "./assets/1.png";
+import p2 from "./assets/2.png";
+import p3 from "./assets/3.png";
+import p4 from "./assets/4.png";
+import p5 from "./assets/5.png";
+import p6 from "./assets/6.png";
+import p7 from "./assets/7.png";
+import p8 from "./assets/8.png";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const pics = [p1, p2, p3, p4, p5, p6, p7, p8];
+  const [cards, setCards] = useState([
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+    ["", "", "", ""],
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen flex flex-col gap-4 justify-center items-center">
+      <div id="board" className="flex flex-col gap-2">
+        {cards.map((row) => {
+          return (
+            <div className="flex gap-2">
+              {row.map((col) => {
+                return <div className="w-[120px] h-[120px] bg-red-200"></div>;
+              })}
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Button variant="contained" color="error" size="large">
+        Reset
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
