c5d10147e7cba1dba0f6cd5492fef22cade05caf
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2024-11-29 14:55:24 +1100

    setup basic board for connect-four

--- connect-four/src/App.jsx
+++ connect-four/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const YELLOW = "";
+const RED = "";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [board, setBoard] = useState([
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+    ["", "", "", "", "", ""],
+  ]);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="flex flex-col w-full h-full justify-center items-center gap-2">
+        {board.map((row) => {
+          return (
+            <div className="flex gap-2">
+              {row.map((col) => {
+                return (
+                  <div className="flex rounded-full bg-red-400 w-[70px] h-[70px]">
+                    {col}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
